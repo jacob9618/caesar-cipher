3f8356c569979a0f87f4445218b2e9dfa414157f
jacob9618 <jacobcampb3lll@gmail.com>
2026-01-08 17:13:18 +0000

    finished encrypt and decrypt

--- script.js
+++ script.js
@@ -8,10 +8,12 @@ function encrypt() {
     let msgValue = document.getElementById("messageInput").value;
 
     // get key value
-    let keyValue = document.getElementById("key").value;
+    let keyValue = parseInt(document.getElementById("key").value);
 
     // create empty result string
     let resultMessage = "";
+
+    let resultBox = document.getElementById("result");
 
     // loop thru msg characters
     for (let i = 0; i < msgValue.length; i++){
@@ -21,18 +23,46 @@ function encrypt() {
         currentChar = currentChar.toLowerCase();
         // convert to number (alpha.indexOf(currentChar))
         currentChar = alpha.indexOf(currentChar);
-        console.log(currentChar);
         // apply formula
-
+        currentChar = (currentChar + keyValue) % 26;
         // convert new character number into a character alpha[encryptedValue]
-
+        currentChar = alpha[currentChar];
         // add encrypted character to a result string
-
+        resultMessage += currentChar;
+        console.log(resultMessage);
         }
     // set encrypted message result text on screen
-
+    resultBox.value = resultMessage;
 }
 
 function decrypt() {
     console.log("decrypt");
+    // get message value
+    let msgValue = document.getElementById("messageInput").value;
+
+    // get key value
+    let keyValue = parseInt(document.getElementById("key").value);
+
+    // create empty result string
+    let resultMessage = "";
+
+    let resultBox = document.getElementById("result");
+
+    // loop thru msg characters
+    for (let i = 0; i < msgValue.length; i++){
+        // get current character (.charAt(i))
+        let currentChar = msgValue.charAt(i);
+        // convert to lowercase (.toLowerCase())
+        currentChar = currentChar.toLowerCase();
+        // convert to number (alpha.indexOf(currentChar))
+        currentChar = alpha.indexOf(currentChar);
+        // apply formula
+        currentChar = (currentChar - keyValue) % 26;
+        // convert new character number into a character alpha[encryptedValue]
+        currentChar = alpha[currentChar];
+        // add encrypted character to a result string
+        resultMessage += currentChar;
+        }
+    // set encrypted message result text on screen
+    resultBox.value = resultMessage;
 }
